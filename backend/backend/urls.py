3611237685a02912642478f7d8ced7b9b9755dbe
Danial <d.bayati28@gmail.com>
2023-01-06 19:41:21 +0330

    Install djoser for authentication user

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+...
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WebNews API",
+        default_version='v1',
+        description="webNews website created by Danial Bayati",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="fahsdani2@gmail.com"),
+        license=openapi.License(name="GPLv3 License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
