330c23cdd1dfdd5ec4665e9556bf3c55943fe9aa
Danial <d.bayati28@gmail.com>
2023-01-09 22:11:46 +0330

    Use poppins font and config heading styles

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,10 @@
 <template>
-  <div class="text-xl text-blue-600">Hello world</div>
+  <div>
+    <h1>Hello world</h1>
+    <h2>Hello world</h2>
+    <h3>Hello world</h3>
+    <h4>Hello world</h4>
+    <h5>Hello world</h5>
+    <h6>Hello world</h6>
+  </div>
 </template>
