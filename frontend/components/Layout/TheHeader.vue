2eb4635f8e8ba5278c1408e8fac384bdfd2c93f3
Danial <d.bayati28@gmail.com>
2023-01-12 22:41:39 +0330

    Create DefaultLayout and styled it

---
+++ frontend/components/Layout/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-20 w-full sticky top-4 sm:top-[60px]">
+    <slot></slot>
+  </div>
+</template>
---
+++ frontend/layouts/DefaultLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    :style="{
+      backgroundImage: `url(/_nuxt/assets/images/bg.webp)`,
+      backgroundPosition: 'fixed',
+    }"
+    class="w-full p-4 overflow-x-hidden"
+  >
+    <div class="container rounded-[40px] m-auto p-4 sm:p-[60px]">
+      <LayoutTheHeader> Hello </LayoutTheHeader>
+      <div class="overflow-y-scroll main animate__animated animate__fadeIn">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+});
+</script>
+
+<style scoped lang="scss">
+.main {
+  height: calc(100vh - 36px);
+  @screen sm {
+    & {
+      height: calc(100vh - 240px);
+    }
+  }
+}
+</style>
