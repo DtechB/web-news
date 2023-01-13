e7c2aaa8a1cbffbf2296b7a18cdcee23f3b876e7
Danial <d.bayati28@gmail.com>
2023-01-13 13:38:35 +0330

    Dynamic category data

--- frontend/pages/categories.vue
+++ frontend/pages/categories.vue
@@ -7,13 +7,38 @@
     <div
       class="grid grid-cols-6 gap-16 justify-items-center bg-slate-900 rounded-xl py-[5%] shadow-xl"
     >
-      <Category v-for="index in 5" :key="index" />
+      <div
+        v-for="item in categories"
+        :key="item.id"
+        :class="{ 'bg-blue-300': item.active }"
+        class="aspect-square col-span-3 md:col-span-2 flex flex-col items-center rounded-2xl border-2 border-blue-300 p-[5%] shadow-blue-300 hover:shadow-md hover:scale-95 hover:shadow-blue-300 transition-all duration-200 md:cursor-pointer"
+        @click="item.active = !item.active"
+      >
+        <div
+          class="w-24 h-24 bg-slate-800 rounded-full flex justify-center items-center"
+        >
+          <BaseIcon :name="item.iconName" size="60px" />
+        </div>
+        <h4 class="mt-3" :class="{ 'text-slate-800': item.active }">
+          {{ item.name }}
+        </h4>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Ref } from "vue";
 definePageMeta({
   layout: "default-layout",
 });
+
+const active: Ref<boolean> = ref(true);
+const categories = ref([
+  { id: 1, name: "Technology", iconName: "technology", active: false },
+  { id: 2, name: "Economics", iconName: "economics", active: false },
+  { id: 3, name: "Politics", iconName: "user", active: true },
+  { id: 4, name: "Health", iconName: "health", active: false },
+  { id: 5, name: "Sports", iconName: "sport", active: true },
+]);
 </script>
