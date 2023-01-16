7e8b1a336a863625117b26d60b317e8c46b00f58
Danial <d.bayati28@gmail.com>
2023-01-16 18:03:47 +0330

    Fix modal image cropper show

--- frontend/pages/news/[id].vue
+++ frontend/pages/news/[id].vue
@@ -4,13 +4,9 @@
       class="bg-slate-900 p-[5%] rounded-lg grid grid-cols-2 justify-items-stretch gap-[5%] mb-4 shadow-lg"
     >
       <div class="col-span-2 xl:col-span-1 rounded-lg">
-        <ImagePicker
-          @onSelect="setPicture"
-          :size="17 / 9"
-          class="animate__animated animate__bounceInLeft"
-        />
-        <div class="mt-10 mb-4 animate__animated animate__bounceInUp">
-          <h3 class="ml-7">Category</h3>
+        <ImagePicker @onSelect="setPicture" :size="17 / 9" />
+        <div class="mt-10 animate__animated animate__bounceInUp">
+          <h3 class="mb-4 ml-7">Category</h3>
           <BaseSelect
             :options="['category', 'name', 'text', 'fake']"
             @on-select="selectCategory"
@@ -23,8 +19,8 @@
           input-name="input-title"
           class="animate__animated animate__bounceInDown"
         />
-        <div class="mt-10 mb-4 animate__animated animate__bounceInRight">
-          <h3 class="mt-10 mb-4 ml-7">Description</h3>
+        <div class="mt-10 animate__animated animate__bounceInRight">
+          <h3 class="mb-4 ml-7">Description</h3>
           <textarea
             rows="7"
             class="border-2 border-blue-300 outline-none focus:border-blue-400 rounded-3xl px-5 py-2 bg-slate-900 w-full text-xl"
